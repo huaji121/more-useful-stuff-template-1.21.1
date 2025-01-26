fed86a41bd9a7b5d10f42d4f101de4f003b92c66
huaji121 <3612354469@qq.com>
2025-01-26 14:28:05 +0800

    第一次提交

---
+++ src/main/java/huaji121/moreusefulstuff/MoreUsefulStuffClient.java
@@ -0,0 +1,10 @@
+package huaji121.moreusefulstuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreUsefulStuffClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/huaji121/moreusefulstuff/MoreUsefulStuffDataGenerator.java
@@ -0,0 +1,11 @@
+package huaji121.moreusefulstuff;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoreUsefulStuffDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
